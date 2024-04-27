e92ccae16e1c99d1d2e83c1d0078b4f1b9a41175
Obake18 <coimbrajuniorm@gmail.com>
2024-04-27 17:59:39 -0300

    bug do reset de fases e trabalho no record.

--- app/runas.js
+++ app/runas.js
@@ -32,20 +32,20 @@ const Runas = () => {
   const [round, setRound] = useState(1);
   const [playerLives, setPlayerLives] = useState(5); // Inicia com 5 vidas
   const [phase, setPhase] = useState(1); // Inicia na fase 1
-  const spinValue = useRef(new Animated.Value(0)).current;
   const [activeIndex, setActiveIndex] = useState(0);
+  const [winStreak, setWinStreak] = useState(0);
+  const [record, setRecord] = useState({ phase: 1, playerElement: null, computerElement: null, round: 1 });
 
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (evt, gestureState) => {
-        spinValue.setValue(gestureState.dx / 100);
-      },
-      onPanResponderRelease: (evt, gestureState) => {
-      },
-    })
-  ).current;
+  const resetGame = () => {
+    setRound(1);
+    setPlayerLives(5);
+    setPhase(1);
+    setPlayerChoice(null);
+    setComputerChoice(null);
+    setResult(null);
+    setRecord({ phase: 1, playerElement: null, computerElement: null, round: 1 }); // Reset the record
+  };
 
   const randomComputerChoice = () => {
     const keys = Object.keys(elementos);
@@ -60,29 +60,44 @@ const Runas = () => {
     setComputerChoice(computer);
     setActiveIndex(Object.keys(elementos).indexOf(elemento));
 
+    // Update the record
+    setRecord({ phase, playerElement: elemento, computerElement: computer, round });
+
+
     if (!computer) {
       setResult('Erro ao selecionar a escolha do computador.');
       return;
     }
-
     if (elemento === computer) {
       setResult('Empate!');
+      setWinStreak(0); // Reset the win streak on a draw
     } else if (elementos[elemento].vence === computer || elementos[computer].perde === elemento) {
       setResult('Você ganhou!');
+      setWinStreak(winStreak + 1); // Increment the win streak on a win
+      if (winStreak >= 2) { // Check if the player has won 3 times in a row
+        setPlayerLives(playerLives + 1); // Give the player an extra life
+        setWinStreak(0); // Reset the win streak
+      }
       if (round % 7 === 0) {
         setPhase(phase + 1);
       }
     } else if (elementos[computer].vence === elemento || elementos[elemento].perde === computer) {
       setResult('Você perdeu!');
+      setWinStreak(0); // Reset the win streak on a loss
       setPlayerLives(playerLives - 1);
-      if (playerLives === 1) {
-        setResult('Game Over');
-        navigation.navigate('GameOver');
-        setRound(1);
-        setPlayerLives(5);
-        setPhase(1);
-      }
+        // Se o jogador não tiver mais vidas, termina o jogo e reseta tudo
+    if (playerLives - 1 === 0) {
+      setResult('Game Over');
+      navigation.navigate('GameOver');
+      setRound(1);
+      setPlayerLives(5);
+      setPhase(1);
     }
+  
+    } else {
+      setResult('Reação desconhecida! Próxima rodada!!')
+    }
+
 
     setRound(round + 1);
     setTimeout(() => {
@@ -111,12 +126,12 @@ const Runas = () => {
       <Image source={require('../assets/imagens/mesa.png')} style={styles.mesa} />
       <Image source={require('../assets/imagens/mara.png')} style={styles.robo} />
       <View style={styles.topSection}>
-  {computerChoice && (
-    <View style={[styles.runa, { backgroundColor: elementos[computerChoice].corBase, position: 'absolute', top: '70%', left: '50%', transform: [{ translateX: -50 }, { translateY: -50 }] }]}>
-      <Text style={styles.kanji}>{elementos[computerChoice].kanji}</Text>
-    </View>
-  )}
-</View>
+        {computerChoice && (
+          <View style={[styles.runa, { backgroundColor: elementos[computerChoice].corBase, position: 'absolute', top: '70%', left: '50%', transform: [{ translateX: -50 }, { translateY: -50 }] }]}>
+            <Text style={styles.kanji}>{elementos[computerChoice].kanji}</Text>
+          </View>
+        )}
+      </View>
       <View style={styles.middleSection}>
         <Text style={styles.title}>Rodada {round}</Text>
         <Text style={styles.resultText}>{result}</Text>
@@ -165,13 +180,13 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   mesa: {
-    marginTop:'40%',
+    marginTop: '40%',
     position: 'absolute',
-    objectFit:'contain'
+    objectFit: 'contain'
   },
   robo: {
-    marginTop:'15%',
-    objectFit:'contain',
+    marginTop: '15%',
+    objectFit: 'contain',
     position: 'absolute',
   },
   middleSection: {
@@ -184,7 +199,7 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'flex-end',
   },
-  
+
   runa: {
     width: 100,
     height: 100,
